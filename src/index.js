d248770fda277aaa2b539804105d348bf04447af
sogood17 <100080482+sogood17@users.noreply.github.com>
2023-02-07 20:52:01 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (input.value !== "") {
+    const li = document.createElement("li");
+    li.innerText = input.value;
+    ul.appendChile(li);
+    input.value = "";
+  }
+});
